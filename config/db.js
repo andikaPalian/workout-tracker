9d244d4213a4604c28ec38d910c497fcbfcf4ee2
Andika Palian <andika12des@gmail.com>
2025-01-19 12:54:19 +0800

    Add database connection setup and update dependencies

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.DATABASE_CONNECTION);
+        console.log(`Database connected: ${connect.connection.host} ${connect.connection.name}`);
+    } catch (error) {
+        console.error("Error connecting to database", error);
+        process.exit(1);
+    }
+};
+
+export default connectDb;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config";
+import connectDb from "./config/db.js";
 
 const app = express();
 const port = process.env.PORT;
+connectDb();
 
 app.use(express.json());
 
